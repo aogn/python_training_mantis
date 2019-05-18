a4e2a632c4834f6290bfc23b3e9fc4885d37e8db
Alena <a.ognienko@gmail.com>
2019-05-18 20:55:43 +0300

    Получение почты по протоколу POP3

--- tests/test_add_new_project.py
+++ tests/test_add_new_project.py
@@ -1,9 +1,15 @@
 from models.project import Project
+import string
+import random
+
+def random_projectname(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 def test_add_new_project(app):
     app.session.login("administrator", "root")
     old_projects = app.project.get_project_list()
-    project = Project(name="Test67e43qwe")
+    project = Project(name=random_projectname("project_", 10))
     app.project.creation(project)
     new_projects = app.project.get_project_list()
     old_projects.append(project)
